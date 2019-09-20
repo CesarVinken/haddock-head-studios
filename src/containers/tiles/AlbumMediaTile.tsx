34c2f68117316560cf5fefab69afa395a698659b
Cesar Vinken <cesarvinken@hotmail.com>
2019-09-20 23:15:49 +0200

    styling changes, especially for viewing wiki on mobile

--- src/containers/tiles/AlbumMediaTile.tsx
+++ src/containers/tiles/AlbumMediaTile.tsx
@@ -1,6 +1,6 @@
-import * as React from "react"
-import { MediaTileProps, MediaTileState, MediaTile } from "./MediaTile"
-import { Link } from "react-router-dom"
+import * as React from 'react'
+import { MediaTileProps, MediaTileState, MediaTile } from './MediaTile'
+import { Link } from 'react-router-dom'
 
 export interface AlbumMediaTileProps extends MediaTileProps {}
 
